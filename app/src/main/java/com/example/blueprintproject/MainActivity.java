f85a61eb0490dc8f5fbbd3541c400040dce61245
Ria Nayar <nayar.ria@gmail.com>
2021-02-21 14:39:45 -0500

    Reset values for the buttons.

--- app/src/main/java/com/example/blueprintproject/MainActivity.java
+++ app/src/main/java/com/example/blueprintproject/MainActivity.java
@@ -26,70 +26,83 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        EditText answerText1 = findViewById(R.id.answer1);
-        EditText answerText2 = findViewById(R.id.answer2);
-        EditText answerText3 = findViewById(R.id.answer3);
-        EditText answerText4 = findViewById(R.id.answer4);
-        Button button = findViewById(R.id.submitButton);
+            EditText answerText1 = findViewById(R.id.answer1);
+            EditText answerText2 = findViewById(R.id.answer2);
+            EditText answerText3 = findViewById(R.id.answer3);
+            EditText answerText4 = findViewById(R.id.answer4);
+            Button button = findViewById(R.id.submitButton);
+            Button reset = findViewById(R.id.reset);
 
-        TextView testText = findViewById(R.id.textViewTest);
+            TextView testText = findViewById(R.id.textViewTest);
 
-        File directory = this.getFilesDir(); // for later
+            File directory = this.getFilesDir(); // for later
 
-        List<String> answers = new ArrayList<>();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                answers.add(answerText1.getText().toString());
-                answers.add(answerText2.getText().toString());
-                answers.add(answerText3.getText().toString());
-                answers.add(answerText4.getText().toString());
-                testText.setText(answers.get(0) + answers.get(1) + answers.get(2) + answers.get(3));
+            List<String> answers = new ArrayList<>();
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    answers.add(answerText1.getText().toString());
+                    answers.add(answerText2.getText().toString());
+                    answers.add(answerText3.getText().toString());
+                    answers.add(answerText4.getText().toString());
+                    testText.setText(answers.get(0) + answers.get(1) + answers.get(2) + answers.get(3));
 
-                try {
-                    int number1 = Integer.parseInt(answers.get(0));
-                    int number2 = Integer.parseInt(answers.get(1));
-                    int number3 = Integer.parseInt(answers.get(2));
-                    int number4 = Integer.parseInt(answers.get(3));
+                    try {
+                        int number1 = Integer.parseInt(answers.get(0));
+                        int number2 = Integer.parseInt(answers.get(1));
+                        int number3 = Integer.parseInt(answers.get(2));
+                        int number4 = Integer.parseInt(answers.get(3));
 
-                    float transit = (float)Math.round((float) (number1 * 0.96)* 100) / 100;
-                    float bottles = (float)Math.round((float) (number2 * .182)* 100) / 100;
-                    float laundry = (float)Math.round((float) (number3 * 0.45)* 100) / 100;
-                    float shower = (float)Math.round((float) (number4 * 1.95)* 100) / 100;
-                    float total = (float)Math.round(transit + bottles + laundry + shower);
+                        float transit = (float) Math.round((float) (number1 * 0.96) * 100) / 100;
+                        float bottles = (float) Math.round((float) (number2 * .182) * 100) / 100;
+                        float laundry = (float) Math.round((float) (number3 * 0.45) * 100) / 100;
+                        float shower = (float) Math.round((float) (number4 * 1.95) * 100) / 100;
+                        float total = (float) Math.round(transit + bottles + laundry + shower);
 
-                    testText.setText("You emitted " + transit + " pounds of CO2 from transit, " + bottles + " pounds of CO2 from plastic bottles " + laundry + " pounds of CO2 from laundry, and " + shower + " pounds from showering!"
-                    + "\n" + "That's a total of " + total + " pounds for the week.");
+                        testText.setText("You emitted " + transit + " pounds of CO2 from transit, " + bottles + " pounds of CO2 from plastic bottles " + laundry + " pounds of CO2 from laundry, and " + shower + " pounds from showering!"
+                                + "\n" + "That's a total of " + total + " pounds for the week.");
 
-                    // save a json
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("Answer1", number1);
-                    jsonObject.put("Answer2", number2);
-                    jsonObject.put("Answer3", number3);
-                    jsonObject.put("Answer4", number4);
+                        // save a json
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put("Answer1", number1);
+                        jsonObject.put("Answer2", number2);
+                        jsonObject.put("Answer3", number3);
+                        jsonObject.put("Answer4", number4);
 
-                    // get date
-                    Date date = Calendar.getInstance().getTime();
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd hh_mm_ss");
-                    String filename = dateFormat.format(date) + ".json";
+                        // get date
+                        Date date = Calendar.getInstance().getTime();
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd hh_mm_ss");
+                        String filename = dateFormat.format(date) + ".json";
 
-                    // save file
-                    String userString = jsonObject.toString();
-                    File file = new File(directory,filename);
-                    FileWriter fileWriter = new FileWriter(file);
-                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                    bufferedWriter.write(userString);
-                    bufferedWriter.close();
+                        // save file
+                        String userString = jsonObject.toString();
+                        File file = new File(directory, filename);
+                        FileWriter fileWriter = new FileWriter(file);
+                        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                        bufferedWriter.write(userString);
+                        bufferedWriter.close();
 
-                } catch (NumberFormatException | JSONException | IOException e) {
-                    testText.setText("Double check - it looks like you didn't enter a number somewhere!");
+                    } catch (NumberFormatException | JSONException | IOException e) {
+                        testText.setText("Double check - it looks like you didn't enter a number somewhere!");
+                    }
                 }
+            });
+            reset.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                answerText1.setText("");
+                answerText2.setText("");
+                answerText3.setText("");
+                answerText4.setText("");
+//                answer1.setText("");
+//                testText.setText("");
+//                testText.setText("");
+//                testText.setText("");
             }
         });
-
     }
 
 }
