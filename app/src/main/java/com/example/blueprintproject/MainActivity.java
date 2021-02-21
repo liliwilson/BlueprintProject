58b1a4b42be807a22b8edb4650f13ba435f72b5a
Lili Wilson <lilimichalwilson@hunterschools.org>
2021-02-21 09:13:50 -0500

    Added recycler view setup to MainActivity.

--- app/src/main/java/com/example/blueprintproject/MainActivity.java
+++ app/src/main/java/com/example/blueprintproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.blueprintproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // use recycler view! Wahoo
+        RecyclerView recyclerView = findViewById(R.id.recycle);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        ClimateQuestionAdapter climateQuestionAdapter = new ClimateQuestionAdapter(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(climateQuestionAdapter);
     }
 
     // Hi it's Ria :)
