1c23b130fc72ce5fa0940cae4278f5ffce8397f2
Lili Wilson <lilimichalwilson@hunterschools.org>
2021-02-21 12:13:05 -0500

    Edited carbon conversions.

--- app/src/main/java/com/example/blueprintproject/MainActivity.java
+++ app/src/main/java/com/example/blueprintproject/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
                     int number3 = Integer.parseInt(answers.get(2));
                     int number4 = Integer.parseInt(answers.get(3));
 
-                    testText.setText("You emitted " + 0.96 * number1 + " pounds of CO2 from transit, " + 2.5 * number3 + " pounds of CO2 from laundry, and " + 1.95*number4 + " pounds from showering!");
+                    testText.setText("You emitted " + 0.96 * number1 + " pounds of CO2 from transit, " + .45 * number3 + " pounds of CO2 from laundry, and " + 1.95*number4 + " pounds from showering!");
                 } catch (NumberFormatException e) {
                     testText.setText("you didn't enter a number.");
                 }
