aaf0fc53e134e701c1b446090f5e1ab6a9675fd0
Lili Wilson <lilimichalwilson@hunterschools.org>
2021-02-21 15:41:28 -0500

    Added a back button.

--- app/src/main/java/com/example/blueprintproject/HistoryActivity.java
+++ app/src/main/java/com/example/blueprintproject/HistoryActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,10 +24,23 @@ public class HistoryActivity extends AppCompatActivity {
         buildRecyclerView();
 
         TextView funFact = findViewById(R.id.textView9);
-        String[] funFacts = {"It takes ~999 gallons of water to make a pair of jeans.", "1 pound of beef requires anywhere between 2000 and 8,000 gallons of water.", "blah.", "LOOK!!!!!"};
+        String[] funFacts = {"It takes ~999 gallons of water to make a pair of jeans.", "1 pound of beef requires anywhere between 2000 and 8,000 gallons of water.", "Heating 1 gallon of water produces, on average, 0.18 lbs of CO2"};
         Random rand = new Random();
         int position = rand.nextInt(funFacts.length);
         funFact.setText(funFacts[position]);
+
+        Button button = findViewById(R.id.backButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startBack();
+            }
+        });
+    }
+
+    public void startBack() {
+        Intent homeActivityIntent = new Intent(this, MainActivity.class);
+        this.startActivity(homeActivityIntent);
     }
 
     private void buildRecyclerView() {
