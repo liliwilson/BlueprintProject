034eb3e46a1900f120d1a8b8f150481badb857e4
Lili Wilson <lilimichalwilson@hunterschools.org>
2021-02-21 15:31:40 -0500

    Tested randomized fun fact.

--- app/src/main/java/com/example/blueprintproject/HistoryActivity.java
+++ app/src/main/java/com/example/blueprintproject/HistoryActivity.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HistoryActivity extends AppCompatActivity {
     ArrayList<String> historyList;
@@ -17,6 +19,12 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
         historyList = (ArrayList<String>) getIntent().getSerializableExtra("history");
         buildRecyclerView();
+
+        TextView funFact = findViewById(R.id.textView9);
+        String[] funFacts = {"It takes ~999 gallons of water to make a pair of jeans.", "1 pound of beef requires anywhere between 2000 and 8,000 gallons of water.", "blah.", "LOOK!!!!!"};
+        Random rand = new Random();
+        int position = rand.nextInt(funFacts.length);
+        funFact.setText(funFacts[position]);
     }
 
     private void buildRecyclerView() {
